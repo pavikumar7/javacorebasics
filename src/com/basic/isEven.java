b3b328c7eccc3cf2aef16325fce6f35aee8c1701
pavikumar <pavithara810@gmail.com>
2018-05-31 17:32:34 +0530

    sample program

---
+++ src/com/basic/isEven.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class isEven {
+public static void main(String v[])
+{
+	isEven t1=new isEven();
+    System.out.print(t1.isEven1(4));
+}
+public boolean isEven1(int one)
+{
+return  one%2==0?true:false;
+}
+}
