347ef4c427774b30f27dc14ccd3d001cfffa268d
pavikumar <pavithara810@gmail.com>
2018-06-01 18:06:18 +0530

    code sample

---
+++ src/com/basic/Taxi.java
@@ -0,0 +1,22 @@
+package com.basic;
+
+public class Taxi  extends Car{
+	
+		public Taxi() {
+			System.out.println("Taxi empty cons");
+		}
+	
+public static void main(String arg[])
+{
+   
+   Taxi t1=new Taxi();
+   System.out.println("empty constructor");
+}
+
+
+
+
+
+	
+
+}
