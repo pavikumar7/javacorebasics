f382ee9ab2a84c00f06a80a697a6537ec7185ad2
pavikumar <pavithara810@gmail.com>
2018-06-04 12:42:43 +0530

    code sample

---
+++ src/com/basic/DataPrimitive.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class DataPrimitive {
+	public static void main(String arg[]) {
+		  int number=5;
+		 String name="pavi" ;
+		   double speed =20.0;
+		    	
+		  System.out.println("speed" + speed);
+		  
+		  System.out.println("number" + number);
+		  System.out.println("name" + name);
+		  
+	}
+
+}
