c3d14391937d66549b70fedde78a087bf5260d59
pavikumar <pavithara810@gmail.com>
2018-06-05 11:03:28 +0530

    sample code the error program

---
+++ src/com/basic/ErrorpPogram.java
@@ -0,0 +1,45 @@
+package com.basic;
+
+public class ErrorpPogram { 
+	public static void main(String arg[]) {
+		
+	
+	
+	 int i = 1;
+	while ( i <= 10 ) {
+		
+	
+	++i;
+	}
+
+
+	for ( double k= 0.1; k <= 1.0; k += 0.1 ) {
+		
+	
+	System.out.println( k );
+
+	int n = 2;
+	switch (n)
+	{
+	case 1:
+	System.out.println( "The number is 1" );
+	case 2:
+	System.out.println( "The number is 2" );
+	break;
+	default:
+	System.out.println( "The number is not 1 or 2" );
+	break;
+	}
+	}
+	
+	
+	
+
+	 System.out.println(" The following code should print the values 1 to 10");
+	 int n = 1;
+	while (n< 10 )
+	System.out.println( n++ );
+
+
+}
+}
