447a7912f1a34d8e25e54e538b1bf0f6c10b6191
pavikumar <pavithara810@gmail.com>
2018-06-05 09:16:01 +0530

    operators example

---
+++ src/com/basic/Operators.java
@@ -0,0 +1,26 @@
+package com.basic;
+
+public class Operators {
+
+
+public static void main(String[] args) {
+	int count=-10;
+	count++;
+	count--;
+     
+      
+	System.out.println(count!=250);
+System.out.println(count==200);
+	System.out.println(count>>2);
+	System.out.println(count>>>2);
+	System.out.println(count<<2);
+	System.out.println(~count);
+	System.out.println(5  &  3);
+	System.out.println(5 |  3);
+	
+	System.out.println(1 ^  2);
+	
+}
+}
+
+
