c5d7fad80d78c60f73f55faf5959e8597f03913b
pavikumar <pavithara810@gmail.com>
2018-06-06 12:48:12 +0530

    code for ReversedArray

---
+++ src/com/basic/Revesedexample.java
@@ -0,0 +1,15 @@
+package com.basic;
+
+public class Revesedexample {
+	public static void main(String[] args) {
+		int[] arr =new int[] {3,4,5,2,33,24,2 };
+
+		
+		for (int i = arr.length - 1; i >= 0; i--) {
+		System.out.println(arr[i]);
+		}
+		}
+}
+
+
+
