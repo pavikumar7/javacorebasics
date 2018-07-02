924846d15e1542c6b8868df07cad8d984f0890ac
pavikumar <pavithara810@gmail.com>
2018-07-02 12:19:41 +0530

    sample code

---
+++ src/com/basic/StudentInfo.java
@@ -0,0 +1,17 @@
+package com.basic;
+
+public class StudentInfo {
+	public static void main(String[] args) {
+		Student s1=new Student(1,"pavi",100);
+		Student s2=new Student(1,"vino",100);
+		Student s3=new Student(1,"tharu",100);
+		Student s4=new Student(1,"suagntamuma",100);
+		s1.getMark();
+		System.out.println(s1.getMark());
+		System.out.println(s2.getMark());
+		System.out.println(s3.getMark());
+		System.out.println(s4.getMark());
+		
+	}
+	 
+ }
