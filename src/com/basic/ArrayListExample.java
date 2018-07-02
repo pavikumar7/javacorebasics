b18c2ee5ac4d7fe6d285f5cae438dde9c0e41f8a
pavikumar <pavithara810@gmail.com>
2018-07-02 12:11:54 +0530

    ArrayListExample.java

---
+++ src/com/basic/ArrayListExample.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		List<String> list= new ArrayList<String>();
+		
+         list.add("one");
+         list.add("two");
+         list.add("three"); 
+         list.add("four");
+         System.out.println(list.get(3));
+         
+         
+		
+		
+	}
+
+}
